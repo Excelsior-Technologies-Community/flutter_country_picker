69226fecde35a76cb0bbbc735420408e5b523230
jaypanchal1014 <jaypanchal1014@gmail.com>
2026-06-15 15:58:25 +0530

    .

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,132 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:flutter_country_picker/flutter_country_picker.dart';
 
 void main() {
-  runApp(
-    const MyApp(),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-  });
+  const MyApp({super.key});
 
   @override
-  Widget build(
-    BuildContext context,
-  ) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:
-          false,
+      debugShowCheckedModeBanner: false,
 
-      home:
-          const HomePage(),
+      home: const HomePage(),
     );
   }
 }
 
-class HomePage
-    extends StatefulWidget {
-
-  const HomePage({
-    super.key,
-  });
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<HomePage> createState() =>
-      _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState
-    extends State<HomePage> {
-
+class _HomePageState extends State<HomePage> {
   Country? selectedCountry;
 
   @override
-  Widget build(
-    BuildContext context,
-  ) {
+  Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        title: const Text(
-          'Country Picker Demo',
-        ),
-      ),
+      appBar: AppBar(title: const Text('Country Picker Demo')),
 
       body: Center(
-
         child: Column(
-          mainAxisAlignment:
-              MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
 
           children: [
-
             if (selectedCountry != null)
-
               Column(
                 children: [
-
                   Text(
-                    selectedCountry!
-                        .flag,
+                    selectedCountry!.flag,
 
-                    style:
-                        const TextStyle(
-                      fontSize: 60,
-                    ),
+                    style: const TextStyle(fontSize: 60),
                   ),
 
-                  const SizedBox(
-                    height: 10,
-                  ),
+                  const SizedBox(height: 10),
 
                   Text(
-                    selectedCountry!
-                        .name,
+                    selectedCountry!.name,
 
-                    style:
-                        const TextStyle(
+                    style: const TextStyle(
                       fontSize: 20,
-                      fontWeight:
-                          FontWeight.bold,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
 
-                  Text(
-                    selectedCountry!
-                        .phoneCode,
-                  ),
+                  Text(selectedCountry!.phoneCode),
                 ],
               ),
 
-            const SizedBox(
-              height: 20,
-            ),
+            const SizedBox(height: 20),
 
             ElevatedButton(
-
               onPressed: () {
-
                 CountryPicker.show(
-
                   context,
 
-                  onSelected:
-                      (country) {
+                  onSelected: (country) {
+                    print(country.name);
+                    print(country.phoneCode);
 
                     setState(() {
-
-                      selectedCountry =
-                          country;
+                      selectedCountry = country;
                     });
                   },
                 );
               },
 
-              child: const Text(
-                'Pick Country',
-              ),
+              child: const Text('Pick Country'),
             ),
           ],
         ),
